1f0adca65069a9ece77963597dc3bb143d3bb7c3
BettinaP <bettinaprophete@gmail.com>
2018-03-07 22:00:28 -0500

    set image select in library to imageView and dismiss UIImagePickerController)

--- Photo Demo/ViewController.swift
+++ Photo Demo/ViewController.swift
@@ -26,11 +26,27 @@ class ViewController: UIViewController,UINavigationControllerDelegate, UIImagePi
         
         let imagePickerController = UIImagePickerController()
         imagePickerController.delegate = self
-        imagePickerController.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        imagePickerController.sourceType = UIImagePickerControllerSourceType.photoLibrary //select camera if accessing camera itself
         imagePickerController.allowsEditing = false
         self.present(imagePickerController, animated: true, completion: nil)
         
     }
 
+    //process image that was selected from photo library
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        //try get image from info(dictionary) that's been passed in as parameter
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            
+            imageView.image = image
+        
+        } else {
+        
+            print("there was a problem getting image")
+        
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+        
+    }
 }
 
